984d0cd26ca10d84f2eb9b61b5b1059877d1ab2e
jbericat <jbericat@uoc.edu>
2021-12-24 15:11:36 +0100

    v05.24

---
+++ src/CNN/PyTorchTraining_Model-v1.py
@@ -0,0 +1,371 @@
+
+"""
+title:: 
+
+
+description::
+
+
+inputs::
+
+
+output::
+
+
+author::
+    Microsoft Docs - Windows AI - Windows Machine Learning
+    Jordi Bericat Ruz - Universitat Oberta de Catalunya
+
+references::
+    1 - https://docs.microsoft.com/en-us/windows/ai/windows-ml/tutorials/pytorch-train-model
+"""
+
+###################################################################################################
+###################################################################################################
+####                                                                                           ####   
+####                                       0. PARAMETRIZATION                                  ####
+####                                                                                           ####   
+###################################################################################################
+###################################################################################################
+
+import os
+import time
+
+# This script's execution timestamp
+EXEC_TIMESTAMP = time.strftime("%Y%m%d-%H%M%S")
+
+# CNN model & Dataset versions
+MODEL_VERSION = 1
+DATASET_VERSION = input("Type the dataset version you want to train the model against (4 = v4.0 / 5 = v5.0 / 6 = v6.0): ")
+
+# model storage path
+MODEL_BIN_PATH = "bin/CNN/CNN-Model-v" + str(MODEL_VERSION) + "_dataset-v" + str(DATASET_VERSION) + "_" + str(EXEC_TIMESTAMP)
+
+# Dataset paths - DEBUG: replace abs for rel paths?
+ROOT_DATA_DIR = 'src/CNN/data/'
+TRAIN_DATA_DIR = os.path.abspath(ROOT_DATA_DIR + 'v' + DATASET_VERSION + '.0/training+validation/')  
+TEST_DATA_DIR = os.path.abspath(ROOT_DATA_DIR + 'v' + DATASET_VERSION + '.0/test/') 
+
+# Data-preparation
+DATASET_IMG_SIZE = 229
+
+#Hyper-parameters
+IMG_CHANNELS = 3
+INPUT_KERNEL = 11
+INNER_KERNEL  = 5
+STRIDE = 1
+PADDING = 1
+
+# the PoC's dataset consists of 500x2=1000 training images and 200x2=400 test images (we're adding the augmented dataset). 
+# Hence, we define a batch size of X to load YY & ZZ batches of images respectively on each epoch:
+BATCH_SIZE = 10
+
+# The number of labels correspond to the number of classes we defined on previous
+# stages of this project. To sum-up, we have: 
+# 
+# - Class #1: High-intensity wildfires 
+# - Class #2: Medium-intensity wildfires 
+# - Class #3: Low-intensity wildfires
+# - Class #4: Images with no wildfires at all
+NUMBER_OF_LABELS = 3 
+
+# opening / creating the file where to store the results
+OUT_FILE = open(MODEL_BIN_PATH + ".info", "w")
+
+# storing all the training environment on the results .info file's preface
+
+OUT_FILE.writelines([ "***************************************\n",
+                      "*** PoC's CNN Model Training Stats  ***\n", 
+                      "***************************************\n\n", 
+                      "Model version: v" + str(MODEL_VERSION) + ".0\n",
+                      "Dataset version: v" + str(DATASET_VERSION) + ".0\n",
+                      "***************************************\n\n",
+                      "HYPER-PARAMETERS:\n\n",
+                      "Image size = (" + str(DATASET_IMG_SIZE) + " x " + str(DATASET_IMG_SIZE) + " x " + str(IMG_CHANNELS) + ")\n",
+                      "Number of classes / labels = "  + str(NUMBER_OF_LABELS) + "\n",
+                      "Input layer kernel size = "  + str(INPUT_KERNEL) + "\n",
+                      "Inner layers kernel size = " + str(INNER_KERNEL) + "\n",
+                      "Stride size = "  + str(STRIDE) + "\n",
+                      "Padding size = "  + str(PADDING) + "\n",
+                      "Batch size = "  + str(BATCH_SIZE) + "\n\n"                     
+                      ])
+
+
+
+###################################################################################################
+###################################################################################################
+####                                                                                           ####   
+####                                       1. IMPORTING THE DATA                               ####
+####                                                                                           ####   
+###################################################################################################
+###################################################################################################
+
+# https://towardsdatascience.com/how-to-apply-a-cnn-from-pytorch-to-your-images-18515416bba1
+
+
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+# 1.1 - Loading and normalizing the data.
+# Define transformations for the training and test sets
+
+transformations = transforms.Compose([
+    transforms.ToTensor(),
+    # Normalizing the images ___________
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    # We need square images to feed the model (the raw dataset has 640x512 size images)
+    # DEBUG - UNCOMMENT NEXT LINE FOR v4 DATASET
+    #transforms.RandomResizedCrop(512),
+    # Now we just resize into any of the common input layer sizes (32×32, 64×64, 96×96, 224×224, 227×227, and 229×229)
+    transforms.Resize(DATASET_IMG_SIZE)
+    ])
+
+augmentations = transforms.Compose([
+transforms.ToTensor(),
+# Normalizing the images ___________
+transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+transforms.RandomHorizontalFlip(p=0.5), # Augmentation techique: Horizontal Mirroring
+# We need square images to feed the model (the raw dataset has 640x512 size images)
+# DEBUG - UNCOMMENT NEXT LINE FOR v4 DATASET
+#transforms.RandomResizedCrop(512),
+# Now we just resize into any of the common input layer sizes (32×32, 64×64, 96×96, 224×224, 227×227, and 229×229)
+transforms.Resize(DATASET_IMG_SIZE)
+])
+
+# 1.2 - Create an instance for training. 
+train_data = datasets.ImageFolder(root=TRAIN_DATA_DIR, transform=transformations) + datasets.ImageFolder(root=TRAIN_DATA_DIR, transform=augmentations)
+
+# 1.3 - Create a loader for the training set which will read the data within batch size and put into memory.
+train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
+print("The number of images in a training set is: ", len(train_loader)*BATCH_SIZE, file=OUT_FILE)
+
+# 1.4 - Create an instance for testing
+test_data = datasets.ImageFolder(root=TEST_DATA_DIR, transform=transformations) + datasets.ImageFolder(root=TEST_DATA_DIR, transform=augmentations)
+
+# 1.5 - Create a loader for the test set which will read the data within batch size and put into memory. 
+# Note that each shuffle is set to false for the test loader.
+test_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
+print("The number of images in a test set is: ", len(test_loader)*BATCH_SIZE, file=OUT_FILE)
+
+print("The number of batches per epoch is: ", len(train_loader), file=OUT_FILE)
+classes = ('high-intensity-wildfire', 'medium-intensity-wildfire', 'low-intensity-wildfire ', 'no-wildfire')
+
+
+###################################################################################################
+###################################################################################################
+####                                                                                           ####   
+####                                       2. DEFINE THE CNN STRUCTURE                         ####
+####                                                                                           ####   
+###################################################################################################
+###################################################################################################
+
+
+# 2.1 - Define a convolution neural network  
+import torch
+import torch.nn as nn
+import torchvision
+import torch.nn.functional as F
+
+class Network(nn.Module):
+     
+    def __init__(self):
+        super(Network, self).__init__()
+    
+        self.conv1 = nn.Conv2d(in_channels=IMG_CHANNELS, out_channels=IMG_CHANNELS*4, kernel_size=INPUT_KERNEL, stride=STRIDE, padding=PADDING)
+        self.bn1 = nn.BatchNorm2d(IMG_CHANNELS*4)
+        self.conv2 = nn.Conv2d(in_channels=IMG_CHANNELS*4, out_channels=IMG_CHANNELS*4, kernel_size=INNER_KERNEL, stride=STRIDE, padding=PADDING)
+        self.bn2 = nn.BatchNorm2d(IMG_CHANNELS*4)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv4 = nn.Conv2d(in_channels=IMG_CHANNELS*4, out_channels=IMG_CHANNELS*8, kernel_size=INNER_KERNEL, stride=STRIDE, padding=PADDING)
+        self.bn4 = nn.BatchNorm2d(IMG_CHANNELS*8)
+        self.conv5 = nn.Conv2d(in_channels=IMG_CHANNELS*8, out_channels=IMG_CHANNELS*8, kernel_size=INNER_KERNEL, stride=STRIDE, padding=PADDING)
+        self.bn5 = nn.BatchNorm2d(IMG_CHANNELS*8)
+        self.fc1 = nn.Linear(264600, NUMBER_OF_LABELS) # The second argument must be the number of classes (4). The first one I'm not so sure, though...
+
+    def forward(self, input):
+        output = F.relu(self.bn1(self.conv1(input)))      
+        output = F.relu(self.bn2(self.conv2(output)))     
+        output = self.pool(output)                        
+        output = F.relu(self.bn4(self.conv4(output)))     
+        output = F.relu(self.bn5(self.conv5(output)))     
+        output = output.view(-1, 264600) # The same as in line 143: I think the second argument could be the size of the receptive field (outputWidth == outputHeight) -> https://medium.com/@RaghavPrabhu/cnn-architectures-lenet-alexnet-vgg-googlenet-and-resnet-7c81c017b848
+        output = self.fc1(output)
+
+        return output
+
+# Instantiate a neural network model 
+model = Network()
+
+# 2.2 - Define a loss function
+from torch.optim import Adam
+ 
+# Define the loss function with Classification Cross-Entropy loss and an optimizer with Adam optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters(), lr=0.0001, weight_decay=0.0001) # setting lr = 0.0001 increases accuracy, but reduces training time significantly
+
+
+###################################################################################################
+###################################################################################################
+####                                                                                           ####   
+####                                         3. TRAIN THE MODEL                                ####
+####                                                                                           ####   
+###################################################################################################
+###################################################################################################
+
+
+from torch.autograd import Variable
+
+# Function to save the model
+def saveModel():
+    torch.save(model.state_dict(), MODEL_BIN_PATH + ".pth")
+
+# Function to test the model with the test dataset and print the accuracy for the test images
+def testAccuracy():
+    
+    model.eval()
+    accuracy = 0.0
+    total = 0.0
+    
+    with torch.no_grad():
+        for images, labels in test_loader: # BUGFIX PR #126 ->  See TASK#05.6: https://github.com/UOC-Assignments/uoc.tfg.jbericat/issues/96
+            images, labels = images.cuda(), labels.cuda() # BUGFIX PR #126 ->  See TASK#05.6: https://github.com/UOC-Assignments/uoc.tfg.jbericat/issues/96
+            # run the model on the test set to predict labels
+            outputs = model(images)
+            # the label with the highest energy will be our prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            accuracy += (predicted == labels).sum().item()
+    
+    # compute the accuracy over all test images
+    accuracy = (100 * accuracy / total)
+    return(accuracy)
+
+# Training function. We simply have to loop over our data iterator and feed the inputs to the network and optimize.
+def train(num_epochs):
+    
+    best_accuracy = 0.0
+
+    # INFO
+    print("\nTraining the model and generating report. Hold-on tight...\n")
+
+    # Define your execution device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("The model will be running on", device, "device",file=OUT_FILE)
+    # Convert model parameters and buffers to CPU or Cuda
+    model.to(device)
+
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        running_acc = 0.0
+
+        for i, (images, labels) in enumerate(train_loader, 0):
+            
+            # get the inputs
+            images = Variable(images.to(device))
+            labels = Variable(labels.to(device))
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # predict classes using images from the training set
+            outputs = model(images)
+            # compute the loss based on model output and real labels
+            loss = loss_fn(outputs, labels)
+            # backpropagate the loss
+            loss.backward()
+            # adjust parameters based on the calculated gradients
+            optimizer.step()
+
+            # Let's print statistics for every 1,000 images
+            running_loss += loss.item()     # extract the loss value
+            if i % 1000 == 999:    
+                # print every 1000 (twice per epoch) 
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 1000), file=OUT_FILE)
+                # zero the loss
+                running_loss = 0.0
+
+        # Compute and print the average accuracy fo this epoch when tested over all 10000 test images
+        accuracy = testAccuracy()
+        print('For epoch', epoch+1,'the test accuracy over the whole test set is %d %%' % (accuracy),file=OUT_FILE)
+        
+        # we want to save the model if the accuracy is the best
+        if accuracy > best_accuracy:
+            saveModel()
+            best_accuracy = accuracy
+
+
+###################################################################################################
+###################################################################################################
+####                                                                                           ####   
+####                                4. TEST THE MODEL ON THE TEST DATA                         ####
+####                                                                                           ####   
+###################################################################################################
+###################################################################################################
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 4.1 - Function to show the images
+def imageshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+
+# 4.2 - Function to test the model with a batch of images and show the labels predictions
+
+def testBatch():
+
+    # get batch of images from the test DataLoader  
+    images, labels = next(iter(test_loader))
+
+    # show all images as one image grid
+    imageshow(torchvision.utils.make_grid(images))
+   
+    # Show the real labels on the screen 
+    print('Real labels: ', ' '.join('%5s' % classes[labels[j]] 
+                               for j in range(BATCH_SIZE)),file=OUT_FILE)
+  
+    # Let's see what if the model identifiers the  labels of those example
+    outputs = model(images)
+    
+    # We got the probability for every 10 labels. The highest (max) probability should be correct label
+    _, predicted = torch.max(outputs, 1)
+    
+    # Let's show the predicted labels on the screen to compare with the real ones
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] 
+                              for j in range(BATCH_SIZE)),file=OUT_FILE)
+
+
+###################################################################################################
+###################################################################################################
+####                                                                                           ####   
+####                                            6. MAIN CODE                                   ####
+####                                                                                           ####   
+###################################################################################################
+###################################################################################################
+
+
+if __name__ == "__main__":
+    
+    # Let's build our model
+    train(5) # DEBUG: WHAT'S WITH THIS "5"!?
+    print('Finished Training')
+
+    # Test which classes performed well
+    # DEBUG - UNCOMMENT NEXT LINE! 
+    #testModelAccuracy()
+    
+    # Let's load the model we just created and test the accuracy per label
+    model = Network()
+    model.load_state_dict(torch.load(MODEL_BIN_PATH + ".pth"))
+
+    # Test with batch of images
+    testBatch()
+
+    # RESULTS REPORTS
+    print("\nTrained model binary file -> " + MODEL_BIN_PATH + ".pth\n")
+    print("Accuracy report and environment details -> " + MODEL_BIN_PATH + ".info\n")
