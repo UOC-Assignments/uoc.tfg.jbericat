f6989cd55c318f2612e1b16b0110cb6e0d2a11f7
jbericat <jbericat@uoc.edu>
2022-01-03 19:11:04 +0100

    v07.16

--- src/AirSim/PythonClient/TFG-PoC/realtime_cv_PoC#1.py
+++ src/AirSim/PythonClient/TFG-PoC/realtime_cv_PoC#1.py
@@ -365,6 +365,7 @@ def poc_one_deploy():
 
     # Let's load the model that got best accuracy during training (86%) for 
     # the PoC-ONE dataset:
+
     model = set_model_version(MODEL_VERSION)
     model.load_state_dict(torch.load(MODEL_PATH + "trained-model.pth"))
 
@@ -372,11 +373,11 @@ def poc_one_deploy():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(" - Model deployed on", device, "device")
 
-    # Convert model parameters and buffers to CPU or Cuda
-    model.to(device)
-
     # Setting evalatuion mode, since we don't want to retrain the model
     model.eval()
+
+    # Convert model parameters and buffers to CPU or Cuda
+    model.to(device)
 
     # DEBUG - THE LOOP STARTS HERE
     # for j in range(len(deploy_data)):
@@ -386,7 +387,7 @@ def poc_one_deploy():
     for i, (images, labels) in enumerate(deploy_loader, 0):
     
         # get batch of images from the test DataLoader  
-        #images, labels = next(iter(deploy_loader)) 
+        images, labels = next(iter(deploy_loader)) 
 
         print("BATCH NUMBER",i)
         
@@ -447,6 +448,132 @@ def poc_one_deploy():
         # TODO - DOC
         add_bounding_box(mydata[i])
 
+def poc_one_deploy_v07_14():
+
+    '''TODO DOCU - Function to test the model with a batch of images and show the labels predictions'''
+
+    # stdout
+    print("\n***************************************************************************************************************************\n" + 
+          " CNN Deployment - PoC #1: Image classification WITHOUT localization (That is, adding bounding boxes to the captured images)" +
+          "\n***************************************************************************************************************************\n")
+
+    #######################################################################
+    # STEP 1: Loading the deployment data 
+    #######################################################################
+
+    # Define transformations for the training and test subsets
+    transformations = transforms.Compose([
+        transforms.ToTensor(),
+        # Normalizing the images ___________
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        transforms.Grayscale(1), # DEBUG -> THIS IS A WORKAROUND; IMAGES ARE EXPECTED TO BE OF ONE CHANNEL ONLY (GRAYSCALE)
+        # We need square images to feed the model (the raw dataset has 640x512 size images)
+        # DEBUG - UNCOMMENT NEXT LINE FOR v4 and v9 DATASETS
+        #transforms.RandomResizedCrop(512),
+        # Now we just resize into any of the common input layer sizes (32×32, 64×64, 96×96, 224×224, 227×227, and 229×229)
+        transforms.Resize(CNN_IMG_SIZE)
+    ])
+
+    # Create an instance for deployment
+    deploy_data = (datasets.ImageFolder(root=DEPLOY_DATA_DIR, transform=transformations))
+    
+    # Create a loader for the test set which will read the data within batch size and put into memory. 
+    # Note that each shuffle is set to false, since we will be creating a frame-by-frame animation 
+    deploy_loader = DataLoader(deploy_data, batch_size=BATCH_SIZE, shuffle=False, num_workers=0) # DEBUG - batch_size=len(deploy_data)
+
+    # TODO -> DOCU
+    classes = ('high-intensity-wildfires', 'low-intensity-wildfires', 'no-wildfires')
+
+    # TODO -> DOCU
+    print( " - The number of images in a deploy set is: " + str(len(deploy_data)) + "\n" )
+    print( " - The batch-size is: " + str(BATCH_SIZE) + "\n" )
+    print( " - The number of batches needed to procees the whole deployment data is:" + str(int(len(deploy_data)//BATCH_SIZE)) + "\n\n" ) # DEBUG - There is a first grade's misscalculation here ^^' use mod op (%) or whatever suits better
+
+    #######################################################################
+    # STEP 2: Loading the CNN model and importing the data  
+    #######################################################################
+
+    # Let's load the model that got best accuracy during training (86%) for 
+    # the PoC-ONE dataset:
+    model = set_model_version(MODEL_VERSION)
+    model.load_state_dict(torch.load(MODEL_PATH + "trained-model.pth"))
+
+    # Define your execution device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(" - Model deployed on", device, "device")
+
+    # Setting evalatuion mode, since we don't want to retrain the model
+    model.eval()
+
+    # Convert model parameters and buffers to CPU or Cuda
+    model.to(device)
+
+    # DEBUG - THE LOOP STARTS HERE
+    # for j in range(len(deploy_data)):
+    mydata = []
+    k = 0 #COUNTER
+    for i, (images, labels) in enumerate(deploy_loader, 0):
+    
+    # get batch of images from the test DataLoader  
+        images, labels = next(iter(deploy_loader)) 
+        
+        # MOVING DATA TO THE GPU MEM SPACE
+        images = Variable(images.to(device))
+        labels = Variable(labels.to(device))
+
+        # show all images as one image grid
+        img_grid = Tensor.cpu(torchvision.utils.make_grid(images))
+        img_grid = img_grid / 2 + 0.5  # unnormalize
+        npimg = img_grid.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.savefig(POC_FOLDER+'out/labels-prediction.png')
+        plt.show()
+
+        #######################################################################
+        # STEP 3: Peforming predictions over the imported data  
+        #######################################################################
+        
+
+        # Let's see what if the model identifies the labels of these example
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+
+        #######################################################################
+        # STEP 4: Showing results  
+        #######################################################################
+
+        # Even though we can't process 
+
+        # Preparing the data to build a table 
+
+        for j in range(len(predicted)):
+
+            # On this implementation (REAL CASE SIMULATION) we actually don't know what
+            # the object classification is, so it must be visually checked afterwards. 
+            # However, we can be sure that the accuracy of the prediction will be of the 86% 
+            # with a small variation index (the model accuracy is pretty stable, 
+            # as seen in the training stats on the section 6.x.x.x). THEREFORE, 
+            # we don't add class labels to the images extracted from the deployment
+            # dataset (labeled as unkown-class on the deployment dataset). Instead, 
+            # we'll be labeling it with the frame/image sequential id (adding the filename 
+            # would be ideal but there is no more time for fancy stuff). This way, an operator 
+            # could verify the validity of a small sample, but big enough to be of significance.
+
+            # assign data
+            frame_id = k # DEBUG: It would be nice to add some formatting (zeroes padding at left, etc)
+            predicted_label = classes[predicted[j]]
+            mydata.append([frame_id, predicted_label])
+            k += 1
+    
+    # create header
+    head = ["Frame ID", "Classification Result"]
+    
+    # print the table to info file
+    print(tabulate(mydata, headers=head, tablefmt="grid"), file=PREDICTIONS_OUT_FILE)
+
+    for i in range(len(deploy_data)): # TODO DEBUG - THIS IS A PATCH! We're creating a myData list of size multiple of the batch-size, instead of the deploy data size
+        # TODO - DOC
+        add_bounding_box(mydata[i])
 
 def main():
 
--- src/CNN/test-code/test_DATASET.py
+++
@@ -1,9 +0,0 @@
-# TEST CODE: Checking the dataset images's number of channels
-
-import cv2
-
-gray_img_path = '/home/jbericat/Workspaces/uoc.tfg.jbericat/usr/raw_datasets/v5.0/test/high-intensity-wildfires/FLIR/FLIR_00000_206.09999_249.29999_-15_270_0_0.png'
-rgb_img_path = '/home/jbericat/Workspaces/uoc.tfg.jbericat/usr/raw_datasets/v5.0/test/high-intensity-wildfires/RGB/RGB_00000_206.09999_249.29999_-15_270_0_0.png'
-
-image = cv2.imread(rgb_img_path, cv2.IMREAD_UNCHANGED)
-print(image.shape)
